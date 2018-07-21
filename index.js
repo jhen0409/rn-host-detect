2805ed14fa05eaa447b556ad67591842f1adda2b
Jhen-Jie Hong <iainst0409@gmail.com>
2018-07-21 22:00:38 +0800

    Fix `window.require` issue on RN 0.56 (#5)
    * Temporarily disable require NativeModules for RN >= 0.56
    
    * Return if get hostname by remote config successful
    
    * window.require => window.require.name
    
    * Use var
    
    * Fix return value of getByRemoteConfig

--- index.js
+++ index.js
@@ -11,34 +11,23 @@ function getByRemoteConfig(hostname) {
   if (
     !Array.isArray(remoteModuleConfig) ||
     hostname !== 'localhost' && hostname !== '127.0.0.1'
-  ) return hostname
+  ) return { hostname: hostname, passed: false }
 
   var constants = (
     remoteModuleConfig.find(getConstants) || []
   )[1]
   if (constants) {
     var serverHost = constants.ServerHost || hostname
-    return serverHost.split(':')[0]
+    return { hostname: serverHost.split(':')[0], passed: true }
   }
-  return hostname
+  return { hostname: hostname, passed: false }
 }
 
 function getConstants(config) {
   return config && (config[0] === 'AndroidConstants' || config[0] === 'PlatformConstants')
 }
 
-/*
- * Get React Native server IP if hostname is `localhost`
- * On Android emulator, the IP of host is `10.0.2.2` (Genymotion: 10.0.3.2)
- */
-module.exports = function (hostname) {
-  if (
-    typeof __fbBatchedBridge !== 'object' ||  // Not on react-native
-    hostname !== 'localhost' && hostname !== '127.0.0.1'
-  ) {
-    return hostname
-  }
-  hostname = getByRemoteConfig(hostname)
+function getByRNRequirePolyfill(hostname) {
   var originalWarn = console.warn
   console.warn = function() {
     if (arguments[0] && arguments[0].indexOf('Requiring module \'NativeModules\' by name') > -1) return
@@ -48,7 +37,14 @@ module.exports = function (hostname) {
   var NativeModules
   var PlatformConstants
   var AndroidConstants
-  if (typeof window === 'undefined' || !window.__DEV__ || typeof window.require !== 'function') {
+  if (
+    typeof window === 'undefined' ||
+    !window.__DEV__ ||
+    typeof window.require !== 'function' ||
+    // RN >= 0.56
+    // TODO: Get NativeModules for RN >= 0.56
+    typeof window.require.name === 'metroRequire'
+  ) {
     return hostname
   }
   NativeModules = window.require('NativeModules')
@@ -67,4 +63,27 @@ module.exports = function (hostname) {
     AndroidConstants.ServerHost
   ) || hostname
   return serverHost.split(':')[0]
+}
+
+/*
+ * Get React Native server IP if hostname is `localhost`
+ * On Android emulator, the IP of host is `10.0.2.2` (Genymotion: 10.0.3.2)
+ */
+module.exports = function (hostname) {
+  // Check if it in React Native environment
+  if (
+    typeof __fbBatchedBridge !== 'object' ||
+    hostname !== 'localhost' && hostname !== '127.0.0.1'
+  ) {
+    return hostname
+  }
+  var result = getByRemoteConfig(hostname)
+
+  // Leave if get hostname by remote config successful
+  if (result.passed) {
+    return result.hostname
+  }
+
+  // Otherwise, use RN's require polyfill
+  return getByRNRequirePolyfill(hostname)
 }
